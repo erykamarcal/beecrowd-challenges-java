707ef1b6589b288eb3b3e5958325fd3e3f4ba4a2
Eryka Marcal <erykamarcalt@gmail.com>
2026-06-12 19:03:39 -0300

    Create arrays package and solve URI 1172

--- src/beecrowd/arrays/Uri1172.java
+++ src/beecrowd/arrays/Uri1172.java
@@ -1,4 +1,25 @@
 package beecrowd.arrays;
 
+import java.util.Scanner;
+
 public class Uri1172 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int [] X = new int[10];
+
+        for (int i = 0; i < X.length; i++) {
+            int numero = scanner.nextInt();
+            if (numero <= 0){
+                numero = 1;
+            }
+            X[i] = numero;
+        }
+
+        for (int i = 0; i < X.length; i++) {
+            System.out.println("X[" + i + "] = " + X[i]);
+        }
+        scanner.close();
+    }
 }
